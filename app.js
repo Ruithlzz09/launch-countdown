35b887fadc8d197cb1b05f84f98b0a6d99f88e9c
Ruithlzz09 <bisht09sachin@gmail.com>
2023-12-16 13:21:19 +0530

    [enhancement]: Add initial loader and changed launch date

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-// Initialise Values for time units i.e. Day, hour, minute and second
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-const timeUnit = [1, day, hour, minute, second];
 // Initialise the event launch date and time
-const countDate = new Date("May 11, 2021 15:13:00").getTime();
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 // CountDown Timer
-const countDown = () => {
-  const now = new Date().getTime();
-  const gap = countDate - now;
-
-  let remainTime = [];
-  for (let index = 0; index < 4; index++) {
-    var remain = Math.floor(
-      (gap % timeUnit[index]) / timeUnit[index + 1]
-    ).toString();
-    remainTime[index] = remain.length === 1 ? "0" + remain : remain;
+const countDown = async() => {
+  const now = new Date()
+  const target = new Date("January 03, 2023 00:00:00")
+  // Calculate the difference in milliseconds
+  const difference = target - now;
+  await delay(3000)
+  loadingScreen.style.display = 'none';
+  document.querySelector('.main-section').style.display='flex'
+  if (difference <= 0) {
+    // Target date has already passed
+    // This condition ensures zero value to be shown in case of the event is already launched
+    // This condition ensures at the time of event launch countdown timer to be stop and latest change
+    console.log("Event has already occurred.")
+    clearInterval(launchTimer);
+    document.querySelector(".coming-soon").classList.add("hide");
+    document.querySelector(".img-section").classList.add("hide");
+    document.querySelector(".launched-site").classList.remove("hide");
+    return
+  }else{
+    document.querySelector(".launched-site").classList.add("hide");
   }
-
-  // This condition ensures zero value to be shown in case of event is already launched
-  remainTime.some((element) => {
-    if (element < 0) {
-      clearInterval(launchTimer);
-    }
-  });
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  const remainTime = [days,hours,minutes,seconds]
 
   document.querySelector(".day").innerText = remainTime[0];
   document.querySelector(".hour").innerText = remainTime[1];
   document.querySelector(".minute").innerText = remainTime[2];
   document.querySelector(".second").innerText = remainTime[3];
-
-  // This condition ensures at the time of event launch countdown timer to be stop and latest change
-  if (gap < 1000) {
-    clearInterval(launchTimer);
-    document.querySelector(".coming-soon").classList.add("hide");
-  }
 };
 
+const loadingScreen = document.querySelector('.loading-screen');
 const launchTimer = setInterval(countDown, 1000);
